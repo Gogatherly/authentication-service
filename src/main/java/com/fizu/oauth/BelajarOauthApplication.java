b2fa29e133558b58e10d959d562bf5919c0db841
Afiss <afisfisabilillah21@gmail.com>
2026-03-12 20:47:22 +0700

    init project

---
+++ src/main/java/com/fizu/oauth/BelajarOauthApplication.java
@@ -0,0 +1,13 @@
+package com.fizu.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizu/oauth/BelajarOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizu.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
